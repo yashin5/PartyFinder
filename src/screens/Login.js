c82c63980ee396307eace16181034c5dca5218c8
Yashin Santos <yashinndsantos@gmail.com>
2018-12-20 22:42:23 -0200

    add categories

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,9 +1,19 @@
 import React, {Component} from 'react'
-import {Image, Dimensions, ImageBackground, Text, View, StyleSheet} from 'react-native';
+import {TouchableOpacity,Image, Dimensions, ImageBackground, Text, View, StyleSheet} from 'react-native';
 
 const width = Dimensions.get('window').width
 
 export default class Login extends Component{
+    constructor(){
+        super();
+        this.state = {icons:{
+            baladas: '../img/dancing.png',
+            raves: '../img/dj.png',
+            rock: '../img/rock.png',
+            cinemas: '../img/ticket.png'
+            
+        }}
+    }
     static navigationOptions = {
         header: null
       }
@@ -11,9 +21,9 @@ export default class Login extends Component{
         return(
             <View style={styles.container}>
                 <ImageBackground 
-                style={ styles.imgBackground }
-                resizeMode='cover' 
-                source={require('../img/party2.jpg')}>
+                    style={ styles.imgBackground }
+                    resizeMode='cover' 
+                    source={require('../img/party2.jpg')}>
                 </ImageBackground>
                 <View style={styles.backgroundTextContainer}>
                     <Text style={styles.titleText}>EAT.</Text>
@@ -27,28 +37,48 @@ export default class Login extends Component{
                     <Text style={styles.navText}>FUTUROS</Text>
                     <Text style={styles.navText}>TODOS</Text>
                 </View>
-                <View style={styles.Categories}>
-                    <Text> Categorias</Text>
-                    <Text> Ver todas</Text>
-                    <View style={styles.navCategories}>
-                        <View>
-                            <Image source={require('../img/dancing.png')}/>
-                            <Text style={styles.navCategoriesText}>Baladas</Text>
-                        </View>
-                        <View>
-                            <Image source={require('../img/dancing.png')}/>
-                            <Text style={styles.navCategoriesText}>Baladas</Text>
-                        </View>
-                        <View>
-                            <Image source={require('../img/dancing.png')}/>
-                            <Text style={styles.navCategoriesText}>Baladas</Text>
-                        </View>
-                        <View>
-                            <Image source={require('../img/dancing.png')}/>
-                            <Text style={styles.navCategoriesText}>Baladas</Text>
-                        </View>
-                        
+                <View style={styles.categoriesContainer}>
+                    <View style={styles.categoriesTitle}>
+                        <Text> Categorias</Text>
+                        <Text> Ver todas  -></Text>
                     </View>
+                    <View style={styles.navCategories}>
+                        <TouchableOpacity>
+                            <View>
+                                <Image source={require('../img/dancing.png')}/>
+                                <Text style={styles.navCategoriesText}>Baladas</Text>
+                            </View>
+                        </TouchableOpacity>
+                        <TouchableOpacity>
+                            <View>
+                                <Image source={require('../img/dj.png')}/>
+                                <Text style={styles.navCategoriesText}>Raves</Text>
+                            </View>
+                        </TouchableOpacity>
+                        <TouchableOpacity>
+                            <View>
+                                <Image source={require('../img/rock.png')}/>
+                                <Text style={styles.navCategoriesText}>Shows</Text>
+                            </View>
+                        </TouchableOpacity>
+                        <TouchableOpacity>
+                            <View>
+                                <Image source={require('../img/ticket.png')}/>
+                                <Text style={styles.navCategoriesText}>Cinema</Text>
+                            </View>           
+                        </TouchableOpacity>         
+                    </View>
+                </View>
+                <View style={styles.showBannerContainer} >
+                    <Image style={styles.showBanner}
+                        source={require('../img/party.jpg')}>
+                    </Image>
+                </View>
+                <View>
+                <Text>DOMINGO, 22 DE SETEMBRO</Text>
+                <Text>MATANZA</Text>
+                <Text>45.000</Text>
+
                 </View>
             </View>
        
@@ -104,17 +134,33 @@ const styles = StyleSheet.create({
     navText: {
 
     },
-    Categories:{
+    categoriesContainer:{
         width: width,
+    },
+    categoriesTitle: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
         marginLeft: 18,
+        marginRight: 18,
+
     },
     navCategories:{
-        width: width,
-        marginLeft: 6,
+        marginLeft: 18,
+        marginRight: 18,
         marginTop: 12,
         flexDirection: 'row',
         justifyContent: 'space-between',
-        
-    }
+     },
+     showBannerContainer: {
+        marginTop: 18,
+        opacity: 0.6,
+     },
+     showBanner: {
+
+
+        borderRadius: 10,
+        width: width-100,
+        height: width-300
+     }
 });
   
